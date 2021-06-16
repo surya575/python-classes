f61a330ca11c17b54d140fa05590557a46e908f5
surya575 <81856529+surya575@users.noreply.github.com>
2021-06-16 11:05:34 +0530

    Add files via upload

---
+++ functions surya.py
@@ -0,0 +1,20 @@
+##def example():
+##    print(10+30)
+##
+##
+##
+##
+##example()
+##
+##example()
+
+
+
+def example(a,b):
+    print(a+b)
+
+ x=int(input('enter a number: '))
+ y=int(input('enter a number: '))
+ exampe(x,y)
+
+ 
