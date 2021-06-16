ccb781f5bfb36b5aa3dbd7b796a00c374651c03e
surya575 <81856529+surya575@users.noreply.github.com>
2021-06-16 10:59:00 +0530

    Add files via upload

---
+++ factorials.py
@@ -0,0 +1,17 @@
+##def fact(n):
+##    if n==1 or n==0:
+##        return 1
+##    else:
+##        return(n*fact(n-1))
+##print(fact(23))
+
+
+
+
+
+def lcm(a,b):
+    if n==0:
+        return 0
+    else:
+        return(a*b)
+print(lcm(2,3))
