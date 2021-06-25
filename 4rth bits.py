29a1a14c2e881c4a3de68043ea954b62893ade13
surya575 <81856529+surya575@users.noreply.github.com>
2021-06-25 15:49:44 +0530

    Add files via upload

---
+++ 4rth bits.py
@@ -0,0 +1,25 @@
+##n=int(input('enter a number: '))
+##rem=0
+##s=''
+##while True:
+##    rem=n%2
+##    print(rem)
+##    s+=str(rem)
+##    n=n//2
+##    if n<=1:
+##        print(1)
+##        s+=str(1)
+##        break
+##
+##s=s[::-1]
+##print('4rth bit',s[3])
+##
+
+
+
+
+
+
+
+      
+      
